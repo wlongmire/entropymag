7245f09705f6cf5803ceca394e3fc58169f6665a
Warren Longmire <warrenlongmire@gmail.com>
2018-09-17 17:24:05 -0400

    all needed data is consumed.

--- process.py
+++ process.py
@@ -36,6 +36,7 @@ import pprint
 conversions = [
     {
         'type': 'deadline',
+        'default': None,
         'entryIndex': 0,
         'regEx': [
             {
@@ -50,6 +51,7 @@ conversions = [
     },
     {
         'type': 'opens',
+        'default': None,
         'entryIndex': 0,
         'regEx': [
             {
@@ -61,6 +63,65 @@ conversions = [
                 'result': lambda entry : datetime.strptime(re.match(r"^Open: (?P<open>.*$)", entry, flags=re.IGNORECASE).group('open'), '%B %d %Y').date()
             }
         ]
+    },
+    {
+        'type': 'genre',
+        'default': ['All-Genres'],
+        'entryIndex': 1,
+        'regEx': [
+            {
+                'reg': r".*",
+                'result': lambda entry : map(lambda value : value.strip(), entry.split('|'))
+            }
+        ]
+    },
+    {
+        'type': 'keywords',
+        'default': [],
+        'entryIndex': 1,
+        'regEx': [
+            {
+                'reg': r".*by (?P<keywords>.* )",
+                'result': lambda entry : re.match(r".*by (?P<keywords>.*)", entry, flags=re.IGNORECASE).group('keywords')
+            },
+            {
+                'reg': r".*as (?P<keywords>.* )",
+                'result': lambda entry : re.match(r".*as (?P<keywords>.*)", entry, flags=re.IGNORECASE).group('keywords')
+            }
+        ]
+    },
+    {
+        'type': 'submission_fee',
+        'default': None,
+        'entryIndex': 2,
+        'regEx': [
+            {
+                'reg': r"\$(?P<fee>\d*)",
+                'result': lambda entry : re.match(r"\$(?P<fee>\d*)", entry, flags=re.IGNORECASE).group('fee')
+            }
+        ]
+    },
+    {
+        'type': 'price',
+        'default': None,
+        'entryIndex': 3,
+        'regEx': [
+            {
+                'reg': r"^Prize: (?P<price>.*)",
+                'result': lambda entry : re.match(r"^Prize: (?P<price>.*)", entry, flags=re.IGNORECASE).group('price')
+            }
+        ]
+    },
+    {
+        'type': 'judges',
+        'default': None,
+        'entryIndex': 4,
+        'regEx': [
+            {
+                'reg': r"^Judges: (?P<judges>.*)",
+                'result': lambda entry : map(lambda entry : entry.strip(), re.match(r"^Judges: (?P<judges>.*)", entry, flags=re.IGNORECASE).group('judges').split('+'))
+            }
+        ]
     }
 ]
 
@@ -69,18 +130,22 @@ conversions = [
 def doConversion(conversion, entryArray):
     entrySearchIndex = conversion['entryIndex']
     conversionRegex = conversion['regEx']
-    entry = entryArray[entrySearchIndex]
 
-    matches = filter(
-        lambda regEntry : re.match(regEntry['reg'], entry, flags=re.IGNORECASE),
-        conversionRegex
-    )
-
-    if len(matches) > 0:
-        #always takes the first match
-        return { conversion['type']: matches[0]['result'](entry) }
+    if (entrySearchIndex + 1 > len(entryArray)):
+        return {conversion['type']: conversion['default']}
     else:
-        return { conversion['type']: None }
+        entry = entryArray[entrySearchIndex]
+
+        matches = filter(
+            lambda regEntry : re.match(regEntry['reg'], entry, flags=re.IGNORECASE),
+            conversionRegex
+        )
+
+        if len(matches) > 0:
+            #always takes the first match
+            return { conversion['type']: matches[0]['result'](entry) }
+        else:
+            return { conversion['type']: None }
 
 #****************
 
@@ -108,5 +173,7 @@ for row in reader:
 
 for idx, entry in enumerate(dict_entries):
     entryObject = map(lambda conversion : doConversion(conversion, entry), conversions)
-    # print entryObject
-    print rows[idx]['org_name']
+    entryObject.append({'org_name': rows[idx]['org_name']})
+    entryObject.append({'url': rows[idx]['url']})
+    entryObject.append({'submission type': rows[idx]['sectionType']})
+    print entryObject[6]
