0e921ba69eacf7153b5c01b43c53ff73e0ab23f2
Warren Longmire <warrenlongmire@gmail.com>
2018-09-17 15:31:15 -0400

    more data cleaning and successfully extracted deadline and opening

--- process.py
+++ process.py
@@ -5,79 +5,108 @@ import csv
 import pprint
 
 
-entry = [
-    'Open: December 30, 2018',
-    'Poetry, Fiction, Nonfiction, Art',
-    '$3'
-]
+# entries = [
+#     [
+#         'Open: December 30, 2018',
+#         'Poetry, Fiction, Nonfiction, Art',
+#         '$3'
+#     ],
+#     [
+#         'Deadline: September 15, 2018',
+#         'Poetry, Fiction, Nonfiction, Art',
+#         '$3'
+#     ],
+#     [
+#         'Deadline: August 22, 2018 - February 1, 2019',
+#         'Poetry, Fiction, Nonfiction, Art',
+#         '$3'
+#     ],
+#     [
+#         'Now',
+#         'Poetry, Fiction, Nonfiction, Art',
+#         '$3'
+#     ],
+#     [
+#         'Year-Round',
+#         'Poetry, Fiction, Nonfiction, Art',
+#         '$3'
+#     ]
+# ]
 
 conversions = [
     {
-        'type': 'Deadline',
+        'type': 'deadline',
         'entryIndex': 0,
         'regEx': [
             {
                 'reg': r"^Deadline: (?P<open>.*) - (?P<deadline>.*$)",
-                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<open>.*) - (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('deadline'), '%B %d, %Y').date()
+                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<open>.*) - (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('deadline'), '%B %d %Y').date()
             },
             {
                 'reg': r"^Deadline: (?P<deadline>.*$)",
-                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('deadline'), '%B %d, %Y').date()
+                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('deadline'), '%B %d %Y').date()
             }
         ]
     },
     {
-        'type': 'Opens',
+        'type': 'opens',
         'entryIndex': 0,
         'regEx': [
             {
                 'reg': r"^Deadline: (?P<open>.*) - (?P<deadline>.*$)",
-                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<open>.*) - (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('open'), '%B %d, %Y').date()
+                'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<open>.*)( )- (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('open'), '%B %d %Y').date()
             },
             {
                 'reg': r"^Open: (?P<open>.*$)",
-                'result': lambda entry : datetime.strptime(re.match(r"^Open: (?P<open>.*$)", entry, flags=re.IGNORECASE).group('open'), '%B %d, %Y').date()
+                'result': lambda entry : datetime.strptime(re.match(r"^Open: (?P<open>.*$)", entry, flags=re.IGNORECASE).group('open'), '%B %d %Y').date()
             }
         ]
     }
 ]
 
-currentConversion = conversions[1]
-entrySearchIndex = currentConversion['entryIndex']
-conversionRegex = currentConversion['regEx']
-entry = entry[entrySearchIndex]
 
-matches = filter(
-    lambda regEntry : re.match(regEntry['reg'], entry, flags=re.IGNORECASE),
-    conversionRegex
-)
+# for any one entry, I want to loop over all conversions
+def doConversion(conversion, entryArray):
+    entrySearchIndex = conversion['entryIndex']
+    conversionRegex = conversion['regEx']
+    entry = entryArray[entrySearchIndex]
 
-if len(matches) > 0:
-    print matches[0]['result'](entry)
-else:
-    print None
+    matches = filter(
+        lambda regEntry : re.match(regEntry['reg'], entry, flags=re.IGNORECASE),
+        conversionRegex
+    )
 
+    if len(matches) > 0:
+        #always takes the first match
+        return { conversion['type']: matches[0]['result'](entry) }
+    else:
+        return { conversion['type']: None }
 
 #****************
 
-# def processBodyEntry(entry):
-#     return entry.strip().decode('ascii', 'ignore').encode("utf-8")
+def processBodyEntry(entry):
+    return entry.decode('ascii', 'ignore').encode("utf-8").strip()
 
-# def filterEntry(entry):
-#     return entry != ''
+def filterEntry(entry):
+    return entry != ''
 
-# cvs_entries = open('entries_clean.csv')
-# reader = csv.DictReader(cvs_entries)
+cvs_entries = open('entries_clean.csv')
+reader = csv.DictReader(cvs_entries)
 
-# dict_entries = []
-# for row in reader:
-#     bodyEntry = filter(
-#         filterEntry,
-#         map(
-#             processBodyEntry,
-#             row['body'].split('/')
-#         )
-#     )
-#     dict_entries.append(bodyEntry)
+dict_entries = []
+rows = []
+for row in reader:
+    bodyEntry = filter(
+        filterEntry,
+        map(
+            processBodyEntry,
+            row['body'].split('/')
+        )
+    )
+    rows.append(row)
+    dict_entries.append(bodyEntry)
 
-# pprint.pprint(dict_entries)
+for idx, entry in enumerate(dict_entries):
+    entryObject = map(lambda conversion : doConversion(conversion, entry), conversions)
+    # print entryObject
+    print rows[idx]['org_name']
