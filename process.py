44791d25a519a03dcc2aad18f00403b5cc32d8ea
Warren Longmire <warrenlongmire@gmail.com>
2018-09-17 13:07:43 -0400

    adding in ability to search and entry for a range of regex expressions and return a custom response.

--- process.py
+++ process.py
@@ -1,25 +1,56 @@
+import re
+from datetime import date
+from datetime import datetime
 import csv
 import pprint
 import unicodedata
 
-def processBodyEntry(entry):
-    return entry.strip().decode('ascii', 'ignore').encode("utf-8")
+# m = re.match(r"(?P<first>\w+) (?P<last>\w+)", "Malcolm Reynolds")
 
-def filterEntry(entry):
-    return entry != ''
+deadlineRE = [
+    {
+        'reg': r"^Deadline: (?P<deadline>.*$)",
+        'result': lambda entry : datetime.strptime(re.match(r"^Deadline: (?P<deadline>.*$)", entry, flags=re.IGNORECASE).group('deadline'), '%B %d, %Y').date()
+    },
+    {
+        'reg': r"^(?P<deadline>Now)",
+        'result': lambda entry : date.today()
+    },
+    {
+        'reg': r"^(?P<deadline>Year-Round)",
+        'result': lambda entry : date.today()
+    }
+]
 
-cvs_entries = open('entries_clean.csv')
-reader = csv.DictReader(cvs_entries)
+entry = "Year-Round" #"Deadline: November 1, 2018"# / Poetry / $30 / Prize: $3,000 + Publication,Presses"
 
-dict_entries = []
-for row in reader:
-    bodyEntry = filter(
-        filterEntry,
-        map(
-            processBodyEntry,
-            row['body'].split('/ ')
-        )
-    )
-    dict_entries.append(bodyEntry)
+deadlineMatches = filter(
+    lambda regEntry :re.match(regEntry['reg'], entry, flags=re.IGNORECASE),
+    deadlineRE)
 
-pprint.pprint(dict_entries)
+if len(deadlineMatches) > 0:
+    print deadlineMatches[0]['result'](entry)
+else:
+    print "Not Found"
+
+# def processBodyEntry(entry):
+#     return entry.strip().decode('ascii', 'ignore').encode("utf-8")
+
+# def filterEntry(entry):
+#     return entry != ''
+
+# cvs_entries = open('entries_clean.csv')
+# reader = csv.DictReader(cvs_entries)
+
+# dict_entries = []
+# for row in reader:
+#     bodyEntry = filter(
+#         filterEntry,
+#         map(
+#             processBodyEntry,
+#             row['body'].split('/')
+#         )
+#     )
+#     dict_entries.append(bodyEntry)
+
+# pprint.pprint(dict_entries)
